303842711aa20ffa1cb72b639920ffd5db898439
kariya <466636883@qq.com>
2023-11-03 13:24:40 +0800

    2023年11月3日 13:24:38

--- my-project/src/views/task/components/details/utils.js
+++ my-project/src/views/task/components/details/utils.js
@@ -9,31 +9,14 @@ export function handlerData(datasource, currentUser, roleList) {
       } else {
         renderData.name = ele.formName
         renderData.type = '总表'
-        if (roleList.indexOf('superAdmin') > -1 || ele.orgResponsibleUser == currentUser.userId || ele.orgContactUser == currentUser.userId) {
-          renderData.children = ele.children.map(child => {
+        renderData.children = ele.children
+          .filter(item => item.isCanFill)
+          .map(child => {
             child.responsibleOrgName = ''
             child.type = '子表'
             child.collaborateOrgName = child.orgName
             return child
           })
-        } else {
-          renderData.children = ele.children
-            .filter(item => {
-              return (
-                item.isCanFill &&
-                (item.orgResponsibleUser == currentUser.userId ||
-                  item.orgContactUser == currentUser.userId ||
-                  item.responsibleUserName == currentUser.username ||
-                  item.fillUserName == currentUser.username)
-              )
-            })
-            .map(child => {
-              child.responsibleOrgName = ''
-              child.type = '子表'
-              child.collaborateOrgName = child.orgName
-              return child
-            })
-        }
       }
       renderDataSource.push(renderData)
     }
