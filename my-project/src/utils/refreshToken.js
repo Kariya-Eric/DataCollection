07770563eed51cbf6b059b07563a6f86119fd8fb
kariya <466636883@qq.com>
2023-11-07 10:27:40 +0800

    2023年11月7日 10:27:38

--- my-project/src/utils/refreshToken.js
+++ my-project/src/utils/refreshToken.js
@@ -1,34 +1,40 @@
-// 用户无操作一小时失效自动退出
-import storage from 'store'
+// 定义最大的闲置时间，例如为3分钟
 import store from '@/store'
+import storage from 'store'
 import { Modal } from 'ant-design-vue'
-import { refreshToken } from '@/api/login'
-import { USER_INFO } from '@/store/mutation-types'
 
-let lastTime = new Date().getTime()
-let currentTime = new Date().getTime()
-let timeOut = 3000 //设置超时时间: 1h
+const MAX_IDLE_TIME = 3600 * 1000
 
-window.onload = function () {
-  window.document.onmousedown = function () {
-    storage.set('lastTime', new Date().getTime())
-  }
+// 记录用户最后一次操作的时间
+let lastActionTime = new Date()
+
+// 定义一个函数，用于每次用户有行为时更新最后操作时间
+function updateLastActionTime() {
+  lastActionTime = new Date()
 }
 
-function checkTimeout() {
-  currentTime = new Date().getTime() //更新当前时间
-  lastTime = storage.get('lastTime')
-  if (currentTime - lastTime > timeOut) {
-    //判断是否超时
-    // 清除storage的数据(登陆信息和token)
-    storage.remove(lastTime)
+// 定义一个计时器
+let timeoutId = setTimeout(() => {
+  if (window.document.location.pathname.indexOf('/user/login') == -1) {
     Modal.confirm({
       title: '登录已过期',
       content: '登陆信息已失效，请重新登录',
-      icon: 'exclamation-circle',
-      cancelButtonProps: { style: { display: 'none' } },
       okText: '重新登录',
       mask: false,
+      icon: 'exclamation-circle',
+      cancelButtonProps: { style: { display: 'none' } },
+      onCancel: () => {
+        store.dispatch('Logout').then(() => {
+          try {
+            let path = window.document.location.pathname
+            if (path != '/' && path.indexOf('/user/login') == -1) {
+              window.location.reload()
+            }
+          } catch (e) {
+            window.location.reload()
+          }
+        })
+      },
       onOk: () => {
         store.dispatch('Logout').then(() => {
           try {
@@ -42,13 +48,49 @@ function checkTimeout() {
         })
       }
     })
-  } else {
-    //刷新token
-    refreshToken({ account: USER_INFO.account }).then(res => {
-      console.log(res)
-    })
   }
-}
+  // 在这里可以添加登出等处理逻辑
+}, MAX_IDLE_TIME)
 
-/* 定时器 间隔30秒检测是否长时间未操作页面 */
-window.setInterval(checkTimeout, 3000)
+// 监听页面上任何一个元素的点击事件
+document.addEventListener('click', () => {
+  if (window.document.location.pathname.indexOf('/user/login') == -1) {
+    updateLastActionTime()
+    clearTimeout(timeoutId)
+    timeoutId = setTimeout(() => {
+      Modal.confirm({
+        title: '登录已过期',
+        content: '登陆信息已失效，请重新登录',
+        okText: '重新登录',
+        mask: false,
+        icon: 'exclamation-circle',
+        cancelButtonProps: { style: { display: 'none' } },
+        onCancel: () => {
+          store.dispatch('Logout').then(() => {
+            try {
+              let path = window.document.location.pathname
+              if (path != '/' && path.indexOf('/user/login') == -1) {
+                window.location.reload()
+              }
+            } catch (e) {
+              window.location.reload()
+            }
+          })
+        },
+        onOk: () => {
+          store.dispatch('Logout').then(() => {
+            try {
+              let path = window.document.location.pathname
+              if (path != '/' && path.indexOf('/user/login') == -1) {
+                window.location.reload()
+              }
+            } catch (e) {
+              window.location.reload()
+            }
+          })
+        }
+      })
+      // 在这里可以添加登出等处理逻辑
+    }, MAX_IDLE_TIME)
+  }
+})
